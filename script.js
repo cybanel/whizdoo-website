27b529270927cedf3dae985bc39aa02fb1e81c7f
Hananel Delcoro <hananel.delcoro@gmail.com>
2026-06-16 20:42:07 +0800

    first Added the fade in transition to all pages

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function toggleMenu() {
   const links = document.getElementById("nav-links");
   links.classList.toggle("open");
 }
+
+/* for fade in animation while scrolling */
+/* fade in on scroll */
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.1 },
+);
+
+document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
